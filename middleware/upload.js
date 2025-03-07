6b5cd6c1696c6396b215a7e0bc3bd0e325754937
dhruv <dhruvgupta.er@gmail.com>
2025-03-07 10:00:52 +0530

    payload size increase

--- middleware/upload.js
+++ middleware/upload.js
@@ -18,6 +18,6 @@ const storage = new CloudinaryStorage({
   }
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage, limits: {filesize: 5*1024*1024} });
 
 module.exports = upload;
