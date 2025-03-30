e185c41faaec0783f48c1858de0321492fb6e101
Divyanshverma1000 <135031159+Divyanshverma1000@users.noreply.github.com>
2025-03-30 14:46:47 +0530

    Jointrip now shows already joined on frontend as well

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -338,7 +338,7 @@ exports.joinTrip = async (req, res) => {
 
     if (trip.members.some((m) => m.user.toString() === req.user.userId)) {
       return res
-        .status(400)
+        .status(200)
         .json({ message: "You have already requested to join this trip" });
     }
 
@@ -358,7 +358,7 @@ exports.joinTrip = async (req, res) => {
       type: "request",
     });
 
-    res.json({ message: "Join request sent successfully", trip });
+    res.status(201).json({ message: "Join request sent successfully", trip });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
