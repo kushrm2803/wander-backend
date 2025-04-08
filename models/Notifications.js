8474aaa939f2b539f39b47f66ba3dec8038671a2
Kush Mahajan <mahajankush2803@gmail.com>
2025-04-08 22:08:24 +0530

    blog ref

--- models/Notifications.js
+++ models/Notifications.js
@@ -4,6 +4,7 @@ const notificationSchema = new mongoose.Schema(
   {
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // For whom the notification is
     tripId: { type: mongoose.Schema.Types.ObjectId, ref: "Trip", required: false }, // Trip for which they are invited
+    blogId: { type: mongoose.Schema.Types.ObjectId, ref: "BlogPost", required: false },
     message: { type: String, required: true },
     isRead: { type: Boolean, default: false },
     requestMadeBy: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: false}, 
