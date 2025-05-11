4d4ef96c0b1606c44255f70b0a4e5548f73e6ef4
Divyanshverma1000 <135031159+Divyanshverma1000@users.noreply.github.com>
2025-05-11 22:08:53 +0530

    Update server.js with 50mb parser limits

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ const { applyDefaults } = require("./models/BlogPost");
 const app = express();
 
 // Middleware
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 app.use(cors());
 app.use(express.static("public"));
 
